b0d7d2238b57c454baeeb8b236c3366861ef0bed
Vasi <vasikaran.25@gmail.com>
2018-02-09 12:23:15 +0530

    Revert "Rajalingam"

--- copy.js
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env node
-const {spawn} = require('child_process');
-const list = spawn('cp', [...process.argv.slice(2)], { "stdio" : "inherit" });
--- help.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-// const {spawn} = require('child_process');
-// const list = spawn('cp', [...process.argv.slice(2)], { "stdio" : "inherit" });
-console.log("crtdir --> Create Folder");
-console.log("cut -- > move a Folder");
-console.log("copy --> copy a folder");
-console.log("lst --> list the folder & file");
