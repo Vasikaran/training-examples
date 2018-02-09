443db669e8177698a148d05ebbe43e2d5073fb0f
vijayk-zutk141 <vijayk-zutk141@tsi.zohocorpin.com>
2018-02-09 14:42:43 +0530

    cli changes

--- src/vijaykumar/first-cli-commands/movefile.js
+++ src/vijaykumar/first-cli-commands/movefile.js
@@ -1,16 +1,16 @@
 #!/usr/bin/env node
-//const { spawn } = require('child_process');
-//const mv = spawn('mv', [...process.argv.slice(2)], {"stdio":"inherit"});
-var arg = process.argv;
-const fs = require('fs');
-const path = require('path');
-if(arg.length == 4){
-  if (fs.existsSync(path.resolve(arg[3])) == false && fs.lstatSync(path.resolve(arg[2])).isDirectory()){
-      fs.renameSync(path.resolve(arg[2]),path.resolve(arg[3]));
-      console.log("folder renamed successfully");
-  }
-  else if(fs.lstatSync(path.resolve(arg[2])).isDirectory()&&fs.lstatSync(path.resolve(arg[3])).isDirectory()){
-    fs.renameSync(path.resolve(arg[2]),path.resolve(arg[3]));
-    console.log("moved successfully");
-  }
-}
+const { spawn } = require('child_process');
+const mv = spawn('mv', [...process.argv.slice(2)], {"stdio":"inherit"});
+// var arg = process.argv;
+// const fs = require('fs');
+// const path = require('path');
+// if(arg.length == 4){
+//   if (fs.existsSync(path.resolve(arg[3])) == false && fs.lstatSync(path.resolve(arg[2])).isDirectory()){
+//       fs.renameSync(path.resolve(arg[2]),path.resolve(arg[3]));
+//       console.log("folder renamed successfully");
+//   }
+//   else if(fs.lstatSync(path.resolve(arg[2])).isDirectory()&&fs.lstatSync(path.resolve(arg[3])).isDirectory()){
+//     fs.renameSync(path.resolve(arg[2]),path.resolve(arg[3]));
+//     console.log("moved successfully");
+//   }
+// }
