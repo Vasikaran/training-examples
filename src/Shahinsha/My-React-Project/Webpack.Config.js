8d8be9fce4fa188fe6774a7d2e62d416c1e7e4b9
shahin-zutk136 <shahin-zutk136@tsi.zohocorpin.com>
2018-02-15 16:29:57 +0530

    Project in Webpack

--- src/Shahinsha/My-React-Project/Webpack.Config.js
+++ src/Shahinsha/My-React-Project/Webpack.Config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const webpack = require("webpack");
 
 module.exports = {
 	entry: './src/index.js',
@@ -7,30 +8,31 @@ module.exports = {
 		path: path.join(__dirname, 'build')
 	},
 	module: {
-		loaders: [
-    {
-      test: /\.jsx?$/,
-      exclude: /node_modules/,
-      loader: 'babel-loader',
-      query: {
-        presets:[ 'es2015', 'react', 'stage-2' ]
-      }
-    }
-  ],
 		rules: [
+			{
+				test: /\.js$/,
+				use: [
+					{
+						loader: 'babel-loader',
+						options: {
+							presets: [
+								require.resolve('babel-preset-es2015'),
+								require.resolve('babel-preset-react'),
+								require.resolve('babel-preset-stage-2')
+							]
+						}
+					},
+					{
+						loader: require.resolve('./loaders/loader.js')
+					}
+				],
+				exclude: path.join(__dirname, 'node_modules')
+			},
 			{
 				test: /\.css$/,
 				use: [
-					{
-						loader: 'style-loader'
-					},
-					{
-						loader: 'css-loader',
-						options: {
-							modules: true,
-							localIdentName: '[name]__[local]'
-						}
-					}
+					"style-loader",
+					"css-loader"
 				]
 			}
 		]
